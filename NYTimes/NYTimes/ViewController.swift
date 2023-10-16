a12f03040f3d09c01d92dd032c116478ee0bdbfe
Samina Shaikh <samina.developer@gmail.com>
2023-10-16 17:20:42 +0400

    Base demo project

---
+++ NYTimes/NYTimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYTimes
+//
+//  Created by Samina Shaikh on 16/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
